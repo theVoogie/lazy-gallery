b1d66e1acc80bf702fbb0b0d608f9659c9da3a8e
Vugar Abdulzada <vugar@origin.no>
2019-01-21 07:37:03 +0100

    Updated to support npm publish

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+require("./styles.css");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+var FirstComponent = function FirstComponent() {
+  return _react.default.createElement("h1", null, "Hello dude!");
+};
+
+var _default = FirstComponent;
+exports.default = _default;
